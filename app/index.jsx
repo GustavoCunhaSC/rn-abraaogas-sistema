9c8828b60cf74de12637eca9d2dd0bc0fa8a7d76
GustavoCunha <gustavo_developer@outlook.com>
2025-08-20 20:53:01 -0300

    rn-template-mobile-app

---
+++ app/index.jsx
@@ -0,0 +1,20 @@
+import { Text, View, StyleSheet, Header } from "react-native";
+
+export default function Index() {
+  return (
+    <View>
+      <Header>
+        <Text>Meu App</Text>
+        
+      </Header>
+    </View>
+  );
+}
+
+// const styles = StyleSheet.create({
+//   paragrafo : {
+//         flex: 1,
+//         justifyContent: "center",
+//         alignItems: "center",
+//   }
+// })
